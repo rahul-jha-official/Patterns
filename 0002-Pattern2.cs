5bda0d1d58899eacbd97ffd95d35f34180f9b733
Rahul Jha <138975150+rahul-jha-official@users.noreply.github.com>
2023-08-06 15:05:53 +0530

    Create 0002-Pattern2.cs

---
+++ 0002-Pattern2.cs
@@ -0,0 +1,33 @@
+/// <pattern>
+///         1
+///       2 1
+///     3 2 1
+///   4 3 2 1
+/// 5 4 3 2 1
+/// </pattern>
+public class Pattern2
+{
+    public void DrawSimple(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int spaces = 1; spaces <= 2 * (n - i);  spaces++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = i; j >= 1; j--)
+            {
+                Console.Write($"{j} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public void DrawMinimal(int n)
+    {
+        for (int i = 1; 
+                i <= n; 
+                Console.WriteLine($"{new string(' ', 2 * (n - i))}{string.Join(" ", Enumerable.Range(1, i).Select(j => i - j + 1))}"),i++
+                );
+    }
+}
