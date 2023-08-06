445147b12509bfcaea2580f6a1698220b4935897
Rahul Jha <138975150+rahul-jha-official@users.noreply.github.com>
2023-08-07 00:06:56 +0530

    Create 0010-Pattern10.cs

---
+++ 0010-Pattern10.cs
@@ -0,0 +1,38 @@
+/// <pattern>
+///        1
+///       1 0 1
+///     1 0 1 0 1
+///   1 0 1 0 1 0 1
+/// 1 0 1 0 1 0 1 0 1
+/// </pattern>
+public class Pattern10
+{
+    public void DrawSimple(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int spaces = 1; spaces <= 2 * (n - i); spaces++)
+            {
+                Console.Write(" ");
+            }
+            bool flag = true;
+            for (int j = 1; j <= i; j++, flag = !flag)
+            {
+                Console.Write($"{(flag ? 1 : 0)} ");
+            }
+            for (int j = 1; j < i; j++, flag = !flag)
+            {
+                Console.Write($"{(flag ? 1 : 0)} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public void DrawMinimal(int n)
+    {
+        for (int i = 1;
+                i <= n;
+                Console.WriteLine($"{new string(' ', 2 * (n - i))}{string.Join(" ", Enumerable.Repeat("1 0", i)).Substring(0, 4 * (i - 1) + 1)}"), i++
+                ) ;
+    }
+}
